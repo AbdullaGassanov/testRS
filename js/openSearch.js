c3debee620136ec11b11f5bf5a18dcc222072ce2
Newgen <reactnewgenjs@gmail.com>
2023-06-04 16:03:52 +0500

    new

--- js/openSearch.js
+++ js/openSearch.js
@@ -2,6 +2,7 @@ const btnSearchOpen = document.querySelector('.header__icon--search');
 const sectionSearch = document.querySelector('.header__brand-search');
 const headerCart = document.querySelector('.header__icon-cart-item');
 const searchInput = document.querySelector('.header__brand-searchTXT');
+const closeBtn = document.querySelector('.header__close-search');
 
 const headerTop = document.querySelector('.header__top');
 
@@ -12,6 +13,9 @@ export const openSearch = function () {
     headerTop.style.justifyContent = 'center';
     headerCart.style.display = 'none';
     sectionSearch.style.display = 'flex';
+    closeBtn.style.display = 'block';
+    sectionSearch.removeAttribute('close', 'close');
+    sectionSearch.setAttribute('open', 'open');
     searchInput.style.backgroundColor = '#000';
     humburg.style.display = 'none';
     btnSearchOpen.style.display = 'none';
--- js/searchCard.js
+++ js/searchCard.js
@@ -1,7 +1,40 @@
 const btnSearch = document.querySelector('.header__brand-btn');
+const btnSearchOpen = document.querySelector('.header__icon--search');
+const sectionSearch = document.querySelector('.header__brand-search');
+const headerCart = document.querySelector('.header__icon-cart-item');
+const searchInput = document.querySelector('.header__brand-searchTXT');
+const headerTop = document.querySelector('.header__top');
+const humburg = document.querySelector('.header__hamburger');
+const closeBtn = document.querySelector('.header__close-search');
 
 export const searchCard = function () {
+  closeBtn.addEventListener('click', () => {
+    console.log('close');
+    sectionSearch.removeAttribute('open', 'open');
+    sectionSearch.setAttribute('close', 'close');
+    headerTop.style.justifyContent = 'space-between';
+    headerCart.style.display = 'block';
+    sectionSearch.style.display = 'none';
+    humburg.style.display = 'block';
+    btnSearchOpen.style.display = 'block';
+    closeBtn.style.display = 'none';
+    console.log('show');
+  });
+
   btnSearch.addEventListener('click', e => {
     console.log(e.target);
+    /* if (
+      searchInput.value == '' ||
+      (searchInput.value == null && sectionSearch.hasAttribute('open'))
+    ) {
+      sectionSearch.removeAttribute('open', 'open');
+      sectionSearch.setAttribute('close', 'close');
+      headerTop.style.justifyContent = 'space-between';
+      headerCart.style.display = 'block';
+      sectionSearch.style.display = 'none';
+      humburg.style.display = 'block';
+      btnSearchOpen.style.display = 'block';
+      console.log('show');
+    } */
   });
 };
