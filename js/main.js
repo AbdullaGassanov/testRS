32e9be8cdf9d6d3cabaad0659feb13970963a11c
Newgen <reactnewgenjs@gmail.com>
2023-07-06 23:11:28 +0500

    email

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/* const CARTT = { apparel: 'Jordan' };
+
+window.order = { id: 'global' }; */
+
+/* window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+}; */
+
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loader');
   loader.classList.add('loader-hidden');
@@ -36,3 +48,104 @@ nav();
 
 openSearch();
 checkPiece();
+
+/* const openCloseNav = function (status, e) {
+  nav.classList.toggle('nav__transform');
+  status == 'open'
+    ? page.classList.add('shadow')
+    : page.classList.remove('shadow');
+  status == 'open' ? main.classList.add('hide') : main.classList.remove('hide');
+  status == 'open'
+    ? (headerAction.style.display = 'none')
+    : (headerAction.style.display = 'flex');
+  status == 'open'
+    ? headerFooter.classList.add('hide')
+    : headerFooter.classList.remove('hide');
+};
+
+navCloseBox.addEventListener('click', () => {
+  openCloseNav('close');
+});
+
+headerHamburger.addEventListener('click', () => {
+  openCloseNav('open');
+});
+
+navClose.addEventListener('click', () => {
+  openCloseNav('close');
+}); */
+
+/* const movingBanner = setInterval(() => {
+	boxImg2.style.transform = "translateX(-100%)";
+	boxImg1.style.transform = "translateX(-100%)";
+}, 4000);
+
+const movingBanner2 = setInterval(() => {
+	boxImg1.style.transform = "translateX(0%)";
+	boxImg2.style.transform = "translateX(0%)";
+}, 8000);
+ */
+
+/* 
+const headerMenu = document.querySelector('.header__menu');
+const headerMenuContent = document.querySelectorAll('.header__menu-content');
+
+headerMenu.addEventListener('mouseover', e => {
+  const event = e.target;
+  if (event.dataset.brand != null) {
+    const brand = event.dataset.brand;
+    console.log(brand);
+    if (event.dataset.brand == brand) {
+      const content = event.nextElementSibling;
+      console.log(content);
+      content.style.opacity = '1';
+      content.style.display = 'block';
+    }
+  }
+});
+
+headerMenu.addEventListener('mouseout', e => {
+  const event = e.target;
+  if (event.dataset.brand != null) {
+    const brand = event.dataset.brand;
+    console.log(brand);
+    if (event.dataset.brand == brand) {
+      const content = event.nextElementSibling;
+      console.log(content);
+      content.style.opacity = '0';
+      content.style.display = 'none';
+    }
+  }
+});
+
+headerMenuContent.forEach(cont => {
+  cont.addEventListener('mouseover', e => {
+    e.target.style.opacity = '1';
+    e.target.display = 'block';
+  });
+});
+
+headerMenuContent.forEach(cont => {
+  cont.addEventListener('mouseout', e => {
+    e.target.style.opacity = '0';
+    e.target.display = 'none';
+  });
+}); */
+
+/* headerMenu.addEventListener('mouseout', e => {
+  const event = e.target;
+  if (event.classList.contains('header__menu-link')) {
+    headerMenuContent.style.opacity = 0;
+    headerMenuContent.style.display = 'none';
+  }
+}); */
+
+/* headerMenuContent.addEventListener('mouseover', e => {
+  headerMenuContent.style.display = 'block';
+  headerMenuContent.style.opacity = 1;
+});
+
+headerMenuContent.addEventListener('mouseout', e => {
+  headerMenuContent.style.display = 'none';
+  headerMenuContent.style.opacity = 0;
+}); */
--- js/neworder.js
+++ js/neworder.js
@@ -1,10 +1,21 @@
-import { nav } from './nav.js';
-import { checkPiece } from './cartItem.js';
-import { openSearch } from './openSearch.js';
-import { openCatalogList } from './linkOpenNavCatalog.js';
-import { searchCard } from './searchCard.js';
-import { preloader } from './_preloader.js';
-import { searchCities } from './_city-seach.js';
+import { nav } from "./nav.js";
+import { checkPiece } from "./cartItem.js";
+import { openSearch } from "./openSearch.js";
+import { openCatalogList } from "./linkOpenNavCatalog.js";
+import { searchCard } from "./searchCard.js";
+import { preloader } from "./_preloader.js";
+import { searchCities } from "./_city-seach.js";
+
+/* function sendRequest() {
+	fetch("https://dog.ceo/api/breed/labrador/images/random")
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+} */
 
 preloader();
 searchCard();
@@ -14,47 +25,49 @@ checkPiece();
 nav();
 searchCities();
 
-const orderContent = document.querySelector('.order__content');
-const orderSucess = document.querySelector('.order__success');
-const orderItems = document.querySelector('.order__items');
-const summary = document.querySelector('.order__summary');
-const orderBtn = document.querySelector('.order__btn');
-const productPieces = localStorage.getItem('numberOfOrders');
-const orders = JSON.parse(localStorage.getItem('order'));
-const cartIcon = document.querySelector('.header__icon-cart-piece');
-const orderItemQuantity = document.querySelector('.order__item-number');
+const orderContent = document.querySelector(".order__content");
+const orderSucess = document.querySelector(".order__success");
+const orderItems = document.querySelector(".order__items");
+const summary = document.querySelector(".order__summary");
+const orderBtn = document.querySelector(".order__btn");
+const orderForm = document.querySelector(".order__form");
+
+const productPieces = localStorage.getItem("numberOfOrders");
+const orders = JSON.parse(localStorage.getItem("order"));
+const cartIcon = document.querySelector(".header__icon-cart-piece");
+const orderItemQuantity = document.querySelector(".order__item-number");
 let sum = 0;
 let quantity = 1;
-let numberOfOrders = JSON.parse(localStorage.getItem('numberOfOrders'));
+let numberOfOrders = JSON.parse(localStorage.getItem("numberOfOrders"));
 
 const renderOrders = function (orderCards) {
-  let idCount = 0;
-  orderItems.innerHTML = '';
-  summary.innerHTML = '';
-  sum = 0;
-  if (orderCards) {
-    orders.forEach(orderCard => {
-      sum += Number(orderCard.price);
+	let idCount = 0;
+	orderItems.innerHTML = "";
+	summary.innerHTML = "";
+	sum = 0;
+	if (orderCards) {
+		orders.forEach((orderCard) => {
+			sum += Number(orderCard.price);
 
-      /* sum.replace(
+			/* sum.replace(
     /\B(?=(\d{3})+(?!\d))/g,
     ' '
   ) */
 
-      orderItems.insertAdjacentHTML(
-        'afterbegin',
-        `
+			orderItems.insertAdjacentHTML(
+				"afterbegin",
+				`
           <div class="order__item" data-id=${idCount++}>
                     <div class="order__item-close"  title="Удалить заказ">x</div>
                     <div class="order__item-content">
                       <img class="order__item-img" src=${
-                        orderCard.img
-                      } alt="img" />
+												orderCard.img
+											} alt="img" />
                       <h2 class="order__item-name">${orderCard.name}</h2>
                       <div class="order__item-calc">
                          <div class="order__item-priceBox"><h2 class="order__item-price">  ${
-                           orderCard.price
-                         } ₽</h2></div> 
+														orderCard.price
+													} ₽</h2></div> 
                         <div class="order__item-quantity"> <button class="order__item-quantity--minus">-</button>
                           <span class="order__item-number">1</span>
                           <button  class="order__item-quantity--plus">+</button> </div>
@@ -63,11 +76,11 @@ const renderOrders = function (orderCards) {
                 </div>
 
 `
-      );
-    });
-    summary.insertAdjacentHTML(
-      'afterbegin',
-      `
+			);
+		});
+		summary.insertAdjacentHTML(
+			"afterbegin",
+			`
             <div class="order__row"> <div class="order__sum-desc">Сумма по товарам</div>
             <h3 class="order__sum"> ${sum} ₽</h3></div>
 
@@ -79,38 +92,38 @@ const renderOrders = function (orderCards) {
 
             </div>
 `
-    );
-  }
+		);
+	}
 };
 
 const calcSum = function () {
-  let amount = parseInt(summary.querySelector('.order__amount').textContent);
-  let summaryVal = [];
+	let amount = parseInt(summary.querySelector(".order__amount").textContent);
+	let summaryVal = [];
 
-  let pricePerItem = orderItems.querySelectorAll('.order__item-price');
-  let quantityPerItem = orderItems.querySelectorAll('.order__item-number');
+	let pricePerItem = orderItems.querySelectorAll(".order__item-price");
+	let quantityPerItem = orderItems.querySelectorAll(".order__item-number");
 
-  let prices = [];
-  let quantities = [];
-  pricePerItem.forEach(p => {
-    prices.push(parseInt(p.textContent));
-  });
-  quantityPerItem.forEach(p => {
-    quantities.push(parseInt(p.textContent));
-  });
+	let prices = [];
+	let quantities = [];
+	pricePerItem.forEach((p) => {
+		prices.push(parseInt(p.textContent));
+	});
+	quantityPerItem.forEach((p) => {
+		quantities.push(parseInt(p.textContent));
+	});
 
-  prices.forEach((price, index) => {
-    summaryVal.push(price * quantities[index]);
-  });
+	prices.forEach((price, index) => {
+		summaryVal.push(price * quantities[index]);
+	});
 
-  amount = summaryVal.reduce((a, b) => a + b);
-  console.log(amount);
+	amount = summaryVal.reduce((a, b) => a + b);
+	console.log(amount);
 
-  summary.innerHTML = '';
+	summary.innerHTML = "";
 
-  summary.insertAdjacentHTML(
-    'afterbegin',
-    `
+	summary.insertAdjacentHTML(
+		"afterbegin",
+		`
             <div class="order__row"> <div class="order__sum-desc">Сумма по товарам</div>
             <h3 class="order__sum"> ${amount} ₽</h3></div>
 
@@ -122,103 +135,83 @@ const calcSum = function () {
 
             </div>
 `
-  );
+	);
 };
 
 renderOrders(orders);
 
-orderItems.addEventListener('click', e => {
-  if (e.target.classList.contains('order__item-quantity--minus')) {
-    let val = Number(e.target.nextElementSibling.textContent);
-    let price = parseInt(
-      e.target
-        .closest('.order__item-quantity')
-        .previousElementSibling.querySelector('.order__item-price').textContent
-    );
+orderItems.addEventListener("click", (e) => {
+	if (e.target.classList.contains("order__item-quantity--minus")) {
+		let val = Number(e.target.nextElementSibling.textContent);
+		let price = parseInt(
+			e.target
+				.closest(".order__item-quantity")
+				.previousElementSibling.querySelector(".order__item-price").textContent
+		);
 
-    if (val != 1) {
-      e.target.nextElementSibling.textContent = --val;
-      calcSum();
-    }
-  }
+		if (val != 1) {
+			e.target.nextElementSibling.textContent = --val;
+			calcSum();
+		}
+	}
 
-  if (e.target.classList.contains('order__item-quantity--plus')) {
-    let price = parseInt(
-      e.target
-        .closest('.order__item-quantity')
-        .previousElementSibling.querySelector('.order__item-price').textContent
-    );
-    let val = Number(e.target.previousElementSibling.textContent);
-    if (val >= 1) {
-      e.target.previousElementSibling.textContent = ++val;
+	if (e.target.classList.contains("order__item-quantity--plus")) {
+		let price = parseInt(
+			e.target
+				.closest(".order__item-quantity")
+				.previousElementSibling.querySelector(".order__item-price").textContent
+		);
+		let val = Number(e.target.previousElementSibling.textContent);
+		if (val >= 1) {
+			e.target.previousElementSibling.textContent = ++val;
 
-      calcSum();
-    }
-  }
-
-  /*   const btnsMinus = orderItems.querySelectorAll('.order__item-quantity--minus');
-  const btnsPlus = orderItems.querySelectorAll('.order__item-quantity--plus');
-
-  btnsMinus.forEach(el => {
-    el.addEventListener('click', e => {
-      console.log(e.target);
-    });
-  });
-
-  btnsPlus.forEach(el => {
-    el.addEventListener('click', e => {
-      console.log('clicked +');
-    });
-  }); */
-
-  /*   if (e.target.classList.contains('order__item-quantity--minus')) {
-    console.log(orderItems.querySelector('.order__item-number'));
-    orderItems.querySelector('.order__item-number').textContent = --quantity;
-  }
-
-  if (e.target.classList.contains('order__item-quantity--plus')) {
-    orderItems.querySelector('.order__item-number').textContent = ++quantity;
-  } */
+			calcSum();
+		}
+	}
 });
 
-orderItems.addEventListener('click', e => {
-  if (e.target.classList.contains('order__item-close')) {
-    console.log(e.target);
-    console.log('delete');
-    let id = e.target.closest('.order__item').dataset.id;
-    console.log(id);
-    e.target.closest('.order__item').remove();
+orderItems.addEventListener("click", (e) => {
+	if (e.target.classList.contains("order__item-close")) {
+		console.log(e.target);
+		console.log("delete");
+		let id = e.target.closest(".order__item").dataset.id;
+		console.log(id);
+		e.target.closest(".order__item").remove();
 
-    orders.splice(id, 1);
+		orders.splice(id, 1);
 
-    console.log(orders);
+		console.log(orders);
 
-    if (numberOfOrders > 0) {
-      --numberOfOrders;
-      cartIcon.textContent = numberOfOrders;
-      localStorage.setItem('numberOfOrders', numberOfOrders);
-    }
+		if (numberOfOrders > 0) {
+			--numberOfOrders;
+			cartIcon.textContent = numberOfOrders;
+			localStorage.setItem("numberOfOrders", numberOfOrders);
+		}
 
-    localStorage.setItem('order', JSON.stringify(orders));
-    renderOrders(orders);
-  }
+		localStorage.setItem("order", JSON.stringify(orders));
+		renderOrders(orders);
+	}
 
-  if (numberOfOrders == 0) {
-    localStorage.removeItem('order');
-    cartIcon.style.visibility = 'hidden';
-  }
+	if (numberOfOrders == 0) {
+		localStorage.removeItem("order");
+		cartIcon.style.visibility = "hidden";
+	}
 });
 
-orderBtn.addEventListener('click', e => {
-  e.preventDefault();
-  console.log('click confirm');
-  console.log(orderContent.querySelector('#order__txtName').value);
-  console.log(orderContent.querySelector('#order__txtPhone').value);
+/* orderBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	console.log("click confirm");
+	console.log(orderContent.querySelector("#order__txtName").value);
+	console.log(orderContent.querySelector("#order__txtPhone").value);
 
-  let name = orderContent.querySelector('#order__txtName').value;
-  let phone = orderContent.querySelector('#order__txtPhone').value;
-  if (name != null && name != '' && phone != 'null' && phone != '') {
-    orderContent.style.display = 'none';
-    orderSucess.style.display = 'flex';
-  }
-});
+	let name = orderContent.querySelector("#order__txtName").value;
+	let phone = orderContent.querySelector("#order__txtPhone").value;
+	if (name != null && name != "" && phone != "null" && phone != "") {
+		orderContent.style.display = "none";
+		orderSucess.style.display = "flex";
+	}
+}); */
+
+/* orderForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+}); */
